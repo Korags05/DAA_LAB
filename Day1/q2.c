236dbb717c643166b46d34ecd34b3beefb19a204
Kunal Saha <kunalsaha1107@gmail.com>
2025-07-29 12:27:03 +0530

    added file handling in q2

--- Day1/q2.c
+++ Day1/q2.c
@@ -5,28 +5,31 @@ arr[0] + arr[1] + arr[2] . . . arr[i].
 */
 
 #include <stdio.h>
+
 int main() {
+    FILE *fp;
     int n;
 
-    //take size of array
-    printf("Enter the size of array: ");
-    scanf("%d", &n);
+    fp = fopen("input2.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    fscanf(fp, "%d", &n);
     int arr[n], prefixSum[n];
 
-    //take input of array
-    for (int i = 0; i<n; i++) {
-        printf("Enter the element [%d]: ", i);
-        scanf("%d", &arr[i]);
+    for (int i = 0; i < n; i++) {
+        fscanf(fp, "%d", &arr[i]);
     }
 
-    //calculate prefix sum
-    //prefixSum[i] will store the sum of elements from arr[0] to arr[i]
+    fclose(fp);
+
     prefixSum[0] = arr[0];
-    for (int i = 1; i<n; i++) {
-        prefixSum[i] = prefixSum[i-1] + arr[i];
+    for (int i = 1; i < n; i++) {
+        prefixSum[i] = prefixSum[i - 1] + arr[i];
     }
 
-    // Print the prefix sum array
     printf("Prefix Sum Array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", prefixSum[i]);
